33778a7e458f4d15d5041d77052d66c2fd16b080
tkopacz <tkopacz@tomaszkopacz.com>
2016-01-16 14:20:38 +0100

    Work

---
+++ TKCopySQL/SQLUpdate.sql
@@ -0,0 +1,33 @@
+﻿drop table [dbo].[tkADFDst]
+drop table [dbo].[tkADFSrc]
+drop procedure [dbo].[spOverwritetkADFDst]
+drop type [dbo].[tkADFSrcType]
+GO
+
+CREATE TABLE [dbo].[tkADFDst] (
+    [data] NVARCHAR (50) NULL
+);
+
+CREATE TABLE [dbo].[tkADFSrc] (
+    [data] NVARCHAR (50) NULL
+);
+
+insert into [dbo].[tkADFSrc]([data]) values ('A1')
+insert into [dbo].[tkADFSrc]([data]) values ('A2')
+insert into [dbo].[tkADFSrc]([data]) values ('A3')
+GO
+
+CREATE TYPE [dbo].[tkADFSrcType] AS TABLE(
+    [data] [nvarchar] (50)
+)
+GO
+
+CREATE PROCEDURE [dbo].[spOverwritetkADFDst] 
+				@tkADFDst [dbo].[tkADFSrcType] READONLY, 
+				@stringData varchar(256), @date datetime2
+AS
+BEGIN
+    --DELETE FROM [dbo].[tkADFDst] -> this can be done as a part od copy process
+    INSERT [dbo].[tkADFDst]([data])
+    SELECT [data] FROM @tkADFDst
+END
---
+++ TKCopySQL/readme.md
@@ -0,0 +1,4 @@
+SqlSink Class
+
+
+https://msdn.microsoft.com/en-us/library/microsoft.azure.management.datafactories.models.sqlsink.aspx
